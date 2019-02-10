43b1bd7701c4803ca870df155a0d180366d08024
Stefan Bueringer <sbueringer@gmail.com>
2019-02-10 10:45:03 +0100

    Removed gmaps, fixed GA, added favicon, refactored layout

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 DIR=$(dirname "$0")
 cd $DIR
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+#if [[ $(git status -s) ]]
+#then
+#    echo "The working directory is dirty. Please commit any pending changes."
+#    exit 1;
+#fi
 
 echo "Deleting old publication"
 rm -rf public
